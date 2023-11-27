6898f888151f312301efc8b722a3d86cfaf5acde
jessubk <jessub.kim@wexinc.com>
2023-11-27 15:06:09 +1100

    initial commmit

---
+++ TestSqlConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using NPoco;
+using NPoco.SqlServer;
+
+Console.WriteLine("Type connection string:");
+string connString = Console.ReadLine();
+
+string sql = "EXECUTE {{stored proc name}} {{parameter list e.g. @@parameter1=@parameter1}}";
+
+var parameters = new
+{
+    //{{parameter1}} = {{paramter1 value}}
+};
+
+IDatabase db = new SqlServerDatabase(connString);
+
+var result = await db.FetchAsync<object>(sql, parameters);
+
+Console.WriteLine("Records returned: " + result.Count);
